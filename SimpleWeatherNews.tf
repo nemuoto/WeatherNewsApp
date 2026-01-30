c19d3e0eb27db44907391ef4985473b201a25137
nemuoto <nemu.oto0106@gmail.com>
2026-01-30 15:48:36 +0900

    　

--- SimpleWeatherNews.tf
+++ SimpleWeatherNews.tf
@@ -199,7 +199,7 @@ resource "aws_apigatewayv2_integration" "lambda" {
 # Routes
 resource "aws_apigatewayv2_route" "get_all" {
   api_id    = aws_apigatewayv2_api.main.id
-  route_key = "GET/all"
+  route_key = "GET /all"
 
   authorization_type = "JWT"
   authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
